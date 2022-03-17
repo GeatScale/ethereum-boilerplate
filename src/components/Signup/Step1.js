0e4ce9d6668a8e44eea6a155826d10e800ff6442
Leonard0Arauj0 <devleonardo99@gmail.com>
2022-03-16 22:39:49 -0400

    Prepared steps confirmation for wallet creation

---
+++ src/components/Signup/Step1.js
@@ -0,0 +1,129 @@
+import React from "react";
+import { Button, Card, Checkbox, Typography } from "antd";
+import { generate_seed } from "utils/seed";
+const { Text } = Typography;
+
+const styles = {
+  title: {
+    fontSize: "20px",
+    fontWeight: "700",
+  },
+  text: {
+    fontSize: "16px",
+  },
+  card: {
+    boxShadow: "0 0.5rem 1.2rem rgb(189 197 209 / 20%)",
+    border: "1px solid #e7eaf3",
+    marginBottom: "30px",
+    borderRadius: "0.5rem",
+  },
+};
+
+const Step1 = ({ changeCheckBox, form, goNextStep, changeForm }) => {
+  function cantGenerateSeed() {
+    return !form.checkbox1 || !form.checkbox2 || !form.checkbox3;
+  }
+
+  function handleGenerateSeed() {
+    if (cantGenerateSeed()) return;
+    changeForm("seed", generate_seed());
+  }
+
+  const dontHaveSeed = form.seed === "";
+
+  return (
+    <div>
+      <Card
+        style={styles.card}
+        title={
+          <>
+            <div>
+              <Text strong>Your recovey phrase is being generated now</Text>
+            </div>
+            <Text style={{ color: "red" }} strong>
+              Do not share your seed with anyone, KEEP YOUR RECOVERY PHRASE
+              (SEED) SAFE!!
+            </Text>
+          </>
+        }
+      >
+        <div style={{ textAlign: "center" }}>
+          <div>
+            <i style={{ fontSize: "30px" }} className="fa-solid fa-lock"></i>
+          </div>
+          <div>
+            <Text strong>
+              We are almost done, check the boxes below and click on Generate my
+              Wallet.
+            </Text>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              margin: "10px",
+            }}
+          >
+            <div>
+              <Checkbox
+                onChange={(e) => changeCheckBox("checkbox1", e)}
+                checked={form.checkbox1}
+              >
+                I understand that all my funds are kept safely in this device,
+                and not on TeiWallet servers.
+              </Checkbox>
+            </div>
+            <div>
+              <Checkbox
+                onChange={(e) => changeCheckBox("checkbox2", e)}
+                checked={form.checkbox2}
+              >
+                I understand that if this application was deleted, my TEI FUNDS
+                can only be recovered with the (json) backup file or my seed
+                phrase.
+              </Checkbox>
+            </div>
+            <div>
+              <Checkbox
+                onChange={(e) => changeCheckBox("checkbox3", e)}
+                checked={form.checkbox3}
+              >
+                I undestand that i will recive a Json file with my seed in it,
+                with this Json file i can easy import my wallet again.
+              </Checkbox>
+            </div>
+          </div>
+          {dontHaveSeed && (
+            <Button
+              style={{ marginBottom: "10px" }}
+              type="primary"
+              onClick={handleGenerateSeed}
+              disabled={cantGenerateSeed()}
+            >
+              Generate my Wallet (SEED)
+            </Button>
+          )}
+        </div>
+        {!dontHaveSeed && (
+          <div>
+            <Card style={{ fontWeight: "bold", textAlign: "center" }}>
+              {form.seed}
+            </Card>
+            <center>
+              <Button
+                style={{ marginTop: "10px" }}
+                type="primary"
+                onClick={goNextStep}
+              >
+                Confirm Wallet
+              </Button>
+            </center>
+          </div>
+        )}
+      </Card>
+    </div>
+  );
+};
+
+export default Step1;
--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,102 +1,43 @@
 import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
-import { Button, Card, Checkbox, Typography } from "antd";
-import { generate_seed } from "utils/seed";
-const { Text } = Typography;
-
-const styles = {
-  title: {
-    fontSize: "20px",
-    fontWeight: "700",
-  },
-  text: {
-    fontSize: "16px",
-  },
-  card: {
-    boxShadow: "0 0.5rem 1.2rem rgb(189 197 209 / 20%)",
-    border: "1px solid #e7eaf3",
-    marginBottom: "30px",
-    borderRadius: "0.5rem",
-  },
-};
+import Step1 from "./Step1";
 
 const Signup = () => {
-  const [seed, setSeed] = useState("");
+  const [form, setForm] = useState({
+    step: 1,
+    seed: "",
+    checkbox1: false,
+    checkbox2: false,
+    checkbox3: false,
+  });
 
-  function handleGenerateSeed() {
-    setSeed(generate_seed());
+  function changeForm(field, newValue) {
+    setForm((current) => {
+      return {
+        ...current,
+        [field]: newValue,
+      };
+    });
+  }
+
+  function changeCheckBox(name, e) {
+    changeForm(name, e.target.checked);
+  }
+
+  function goNextStep() {
+    changeForm("step", form.step + 1);
   }
 
   return (
     <div>
       <h1 style={{ textAlign: "center", marginBottom: "12px" }}>Signup</h1>
-      <div style={{ display: "flex", gap: "10px" }}></div>
-      <Card
-        style={styles.card}
-        title={
-          <>
-            <div>
-              <Text strong>
-                Your recovey phrase (Seed) is being generated now
-              </Text>
-            </div>
-            <Text style={{ color: "red" }} strong>
-              Do not share your seed with anyone, KEEP YOUR RECOVERY PHRASE
-              (SEED) SAFE!!
-            </Text>
-          </>
-        }
-      >
-        <div style={{ textAlign: "center" }}>
-          <div>
-            <i style={{ fontSize: "30px" }} className="fa-solid fa-lock"></i>
-          </div>
-          <div>
-            <Text strong>
-              We are almost done, check the boxes below and click on Generate my
-              Wallet (SEED)
-            </Text>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "flex-start",
-              margin: "10px",
-            }}
-          >
-            <div>
-              <Checkbox>
-                I understand that all my funds are kept safely in this device,
-                and not on TeiWallet servers.
-              </Checkbox>
-            </div>
-            <div>
-              <Checkbox>
-                I understand that if this application was deleted, my TEI fUNDS
-                can only be recovered with the (json) backup file or my seed
-                phrase.
-              </Checkbox>
-            </div>
-            <div>
-              <Checkbox>
-                I undestand that i will recive a Json file with my seed in it,
-                with this Json file i can easy import my wallet again.
-              </Checkbox>
-            </div>
-          </div>
-          <Button
-            style={{ marginBottom: "10px" }}
-            type="primary"
-            onClick={handleGenerateSeed}
-          >
-            Generate my Wallet (SEED)
-          </Button>
-        </div>
-        <Card data-cy="seed-box">
-          <p>{seed}</p>
-        </Card>
-      </Card>
+      {form.step === 1 && (
+        <Step1
+          changeCheckBox={changeCheckBox}
+          form={form}
+          goNextStep={goNextStep}
+        />
+      )}
     </div>
   );
 };
