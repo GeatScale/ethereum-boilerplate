8cbc586113ad659d2505d5e3ba739ca283b6b9cb
Leonard0Arauj0 <devleonardo99@gmail.com>
2022-03-18 21:38:16 -0400

    saveKeyStore function creation

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { ethers } from "ethers";
 import { withRouter } from "react-router-dom";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
+import { HD_PATH } from "constants/wallet";
 
 const Signup = () => {
   const [form, setForm] = useState({
@@ -16,6 +18,17 @@ const Signup = () => {
   });
 
   const minCharacters = 6;
+
+  async function saveKeyStore() {
+    if (ethers.utils.isValidMnemonic(form.seed)) {
+      const wallet = ethers.Wallet.fromMnemonic(form.seed);
+      const encryptedJSON = await wallet.encrypt(form.password, {
+        path: HD_PATH,
+      });
+      localStorage.setItem("KEY_STORE", encryptedJSON);
+      localStorage.setItem("LOGGED", true);
+    }
+  }
 
   function changeForm(field, newValue) {
     setForm((current) => {
@@ -46,20 +59,14 @@ const Signup = () => {
         />
       )}
       {form.step === 2 && (
-        <Step2
-          form={form}
-          changeForm={changeForm}
-          changeCheckBox={changeCheckBox}
-          goNextStep={goNextStep}
-        />
+        <Step2 form={form} changeForm={changeForm} goNextStep={goNextStep} />
       )}
       {form.step === 3 && (
         <Step3
           form={form}
           changeForm={changeForm}
-          changeCheckBox={changeCheckBox}
-          goNextStep={goNextStep}
           minCharacters={minCharacters}
+          saveKeyStore={saveKeyStore}
         />
       )}
     </div>
