72b9a38e72bc47f6f36bdb9a6b76087f3eb15841
Panqueca <reyrogerio@hotmail.com>
2022-03-20 20:46:42 -0300

    Removed refresh balance

--- src/components/NativeBalance.jsx
+++ src/components/NativeBalance.jsx
@@ -1,38 +1,10 @@
-import { useEffect } from "react";
-
 import { useMoralis, useNativeBalance } from "react-moralis";
 import { useAuthState } from "store/auth/state";
 
 function NativeBalance() {
   const { account, chainId } = useMoralis();
-  const {
-    data: balance,
-    getBalances,
-    isLoading,
-  } = useNativeBalance({ chain: chainId });
+  const { data: balance } = useNativeBalance({ chain: chainId });
   const { isAuthenticated } = useAuthState();
-
-  async function refreshBalance() {
-    function reset() {
-      setTimeout(() => {
-        refreshBalance();
-      }, 15000);
-    }
-
-    try {
-      if (!isLoading) {
-        await getBalances();
-        reset();
-      }
-    } catch (err) {
-      reset();
-    }
-  }
-
-  useEffect(() => {
-    refreshBalance();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   if (!account || !isAuthenticated) return null;
 
