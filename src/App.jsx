5eb8d06f02edf32bc2fb2e3f971e14ae46ba3fd2
Leonard0Arauj0 <devleonardo99@gmail.com>
2022-03-21 21:21:10 -0400

    Remove bsc and polygon from swap options

--- src/App.jsx
+++ src/App.jsx
@@ -93,12 +93,6 @@ const App = () => {
                 <Tabs.TabPane tab={<span>Ethereum</span>} key="1">
                   <DEX chain="eth" />
                 </Tabs.TabPane>
-                <Tabs.TabPane tab={<span>Binance Smart Chain</span>} key="2">
-                  <DEX chain="bsc" />
-                </Tabs.TabPane>
-                <Tabs.TabPane tab={<span>Polygon</span>} key="3">
-                  <DEX chain="polygon" />
-                </Tabs.TabPane>
               </Tabs>
             </Route>
             <Route path="/backup">
